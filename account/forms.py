17f0bfe11eb7658a81bcee058b36440d7a93c65b
Mohammed Manahi <mohammedsaadmanahi@hotmail.com>
2023-01-26 19:07:48 +0300

    Fix ui issues in edit account and profile pages

--- account/forms.py
+++ account/forms.py
@@ -7,10 +7,12 @@ class UserEditForm(forms.ModelForm):
     """
     Create user edit form to edit default user fields
     """
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': "form-control"}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': "form-control"}))
 
     class Meta:
         model = User
-        fields = ["first_name", "last_name"]
+        fields = ['first_name', 'last_name']
 
 
 class LoginForm(forms.Form):
@@ -51,11 +53,14 @@ class ProfileForm(forms.ModelForm):
     """
     Create profile model form
     """
+    photo = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-primary'}))
 
     class Meta:
         model = Profile
         fields = ['date_of_birth', 'photo', 'favorite']
-        widgets = {'date_of_birth': forms.widgets.DateInput(attrs={'type': 'date'})}
+        widgets = {
+            'date_of_birth': forms.widgets.DateInput(
+                attrs={'type': 'date', 'class': 'form-control flatpickr flatpickr-input form-control active'})}
 
     def clean_date_of_birth(self):
         """
